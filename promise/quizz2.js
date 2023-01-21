56f23b93b69e0f893cf2ff06f2ccbb9ccbfce84a
minsik0726 <kimminsik0726@gmail.com>
2023-01-21 16:55:12 +0900

    Feat: promise/quizz2 구현

--- promise/quizz2.js
+++ promise/quizz2.js
@@ -43,3 +43,53 @@ console.log("비동기 요청 3")
       결과 값으로는 반드시 "정상적으로 실행되었습니다"가 출력되어야한다
       그러나, 3가지 요청이 모두 실패했을 때는 "결과값을 가지고 오는데 실패하였습니다"가 출력되어야한다.
 */
+
+// 기초
+/*
+      number의 조건에 따라 비동기 요청 1,2,3이 성공과 실패를 하게 되도록 조건을 추가합니다
+      요청 1은 number가 100보다 작은 경우에만 성공,
+      요청 2는 number가 50보다 작은 경우에만 성공,
+      요청 3은 number가 10보다 작은 경우에만 성공하도록 number에 다른 수를 입력합니다
+*/
+let number = 15;
+const request1 = new Promise(function (resolve, reject) {
+  setTimeout(() => {
+    if (number < 100) {
+      resolve("비동기 요청 1");
+    } else {
+      reject(new Error("결과값을 가져오는데 실패했습니다"));
+    }
+  }, 1000);
+});
+const request2 = new Promise(function (resolve, reject) {
+  setTimeout(() => {
+    if (number < 50) {
+      resolve("비동기 요청 2");
+    } else {
+      reject(new Error("결과값을 가져오는데 실패했습니다"));
+    }
+  }, 1000);
+});
+const request3 = new Promise(function (resolve, reject) {
+  setTimeout(() => {
+    if (number < 10) {
+      resolve("비동기 요청 3");
+    } else {
+      reject(new Error("결과값을 가져오는데 실패했습니다"));
+    }
+  }, 1000);
+});
+
+const requestAll = async () => {
+  try {
+    const requestArray = await Promise.all([request1, request2, request3]);
+    console.log(requestArray[0]);
+    console.log(requestArray[1]);
+    console.log(requestArray[2]);
+    console.log("정상적으로 실행되었습니다");
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+requestAll();
