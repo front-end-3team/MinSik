4427612a3c1638b2b67faee6e5de8b251b4e857d
minsik0726 <kimminsik0726@gmail.com>
2023-02-13 14:53:21 +0900

    Fix: input에 value 값 추가
    input에 value 어트리뷰트를 추가하여 항상 input의 값 또한 상태가 변경될 때마다 업데이트되도록 수정하였습니다.

--- practice/src/InputTest/input.js
+++ practice/src/InputTest/input.js
@@ -1,7 +1,7 @@
 function Input({ text, changeText, clearText }) {
   return (
     <>
-      <input onChange={changeText} />{" "}
+      <input onChange={changeText} value={text} />
       <button onClick={clearText}>초기화</button>
       <div>
         <p>Input의 값: {text}</p>
