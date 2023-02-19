5e2502e5b53d434f7a61e03109d79134d43cd9cd
minsik0726 <kimminsik0726@gmail.com>
2023-02-19 21:38:36 +0900

    Feat: state 1 기능 구현

--- hookstest/Hooks/src/hooks/2.state/state1.js
+++ hookstest/Hooks/src/hooks/2.state/state1.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import PlayListMock from "../../__mock__/playList.json";
 
 function State1() {
@@ -14,29 +15,70 @@ function State1() {
   console.log(PlayListMock.playlist);
   /* 데이터 콘솔에 찍어두었으니 확인해볼 것 */
 
+  const [lists, setLists] = useState(PlayListMock.playlist);
+
+  const [inputs, setInputs] = useState({
+    title: "",
+    signer: "",
+  });
+
+  const { title, signer } = inputs;
+
+  const onInputChange = (e) => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+
+  const onAddMusic = () => {
+    const list = {
+      title,
+      signer,
+    };
+
+    setLists([...lists, list]);
+
+    setInputs({
+      title: "",
+      signer: "",
+    });
+  };
+
+  const onRemoveMusic = (title) => {
+    setLists(lists.filter((list) => list.title !== title));
+  };
+
   return (
     <>
       <h1>문제1</h1>
       <ul>
-        {PlayListMock.playlist.map((v) => (
+        {lists.map((v) => (
           <li>
             <h3>{v.title}</h3>
             <p>{v.signer}</p>
+            <button
+              onClick={() => {
+                onRemoveMusic(v.title);
+              }}
+            >
+              삭제
+            </button>
+            <hr></hr>
           </li>
         ))}
       </ul>
       <div>
         <p>
-          곡명 : <input />
+          곡명 : <input name="title" value={title} onChange={onInputChange} />
         </p>
         <p>
-          가수/작곡 : <input />
+          가수/작곡 :
+          <input name="signer" value={signer} onChange={onInputChange} />
         </p>
         <p>
-          <button>추가</button>
-        </p>
-        <p>
-          <button>삭제</button>
+          <button onClick={onAddMusic}>추가</button>
         </p>
       </div>
     </>
