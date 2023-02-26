2d13e08ee8834f477d379ef182a0e69fe4e526d4
minsik0726 <kimminsik0726@gmail.com>
2023-02-26 23:27:48 +0900

    Feat: state 3 없는 번호 상품으로 접근 시 state로 복귀

--- hookstest/Hooks/src/pages/Detail.js
+++ hookstest/Hooks/src/pages/Detail.js
@@ -1,7 +1,22 @@
-import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import productList from "../__mock__/products.json";
+
 function DetailPage() {
   const params = useParams();
-  console.log(params.productNumber);
+
+  const product = productList.products.find(
+    (product) => product.productNumber === params.productNumber
+  );
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!product) {
+      alert("없는 상품 번호입니다");
+      navigate("/state");
+    }
+  }, []);
 
   return (
     <div>
